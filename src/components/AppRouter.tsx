04b8dce079d3d7ba6aed038801c60a264ef79a19
btwidc <krauchankakiryl@tut.by>
2022-01-31 13:04:58 +0300

    feat: create page component for login and registration

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Auth from "../pages/Auth";
-import Registration from "../pages/Registration";
 import { LOGIN_ROUTE, REGISTRATION_ROUTE } from "../utils/routesPath";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path={LOGIN_ROUTE} element={<Auth />} />
-      <Route path={REGISTRATION_ROUTE} element={<Registration />} />
+      <Route path={REGISTRATION_ROUTE} element={<Auth />} />
       <Route path="*" element={<Auth />} />
     </Routes>
   );
--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,7 +1,75 @@
 import React from "react";
+import "../css-modules/Auth.css";
+import { NavLink, useLocation } from "react-router-dom";
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from "../utils/routesPath";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const location = useLocation();
+  const isRegistration = location.pathname === REGISTRATION_ROUTE;
+  return (
+    <div className="limiter">
+      <div className="auth-form-container">
+        <div className="auth-form">
+          <form className="auth-form-content">
+            <span className="auth-form-title">
+              {isRegistration ? "Registration" : "Login"}
+            </span>
+            <div className="auth-form-item">
+              <span className="auth-form-item-title">E-mail</span>
+              <input
+                className="auth-form-item-field"
+                type="text"
+                name="email"
+                placeholder="Type your email"
+              />
+            </div>
+            <div className="auth-form-item">
+              <span className="auth-form-item-title">Password</span>
+              <input
+                className="auth-form-item-field"
+                type="password"
+                name="password"
+                placeholder="Type your password"
+              />
+            </div>
+            {isRegistration && (
+              <div className="auth-form-item">
+                <span className="auth-form-item-title">Confirm Password</span>
+                <input
+                  className="auth-form-item-field"
+                  type="password"
+                  name="passwordConfirm"
+                  placeholder="Confirm your password"
+                />
+              </div>
+            )}
+            <div className="auth-form-link-field">
+              {isRegistration ? (
+                <NavLink
+                  className="auth-form-registration-link"
+                  to={LOGIN_ROUTE}
+                >
+                  Already have an account? Sign in!
+                </NavLink>
+              ) : (
+                <NavLink
+                  className="auth-form-registration-link"
+                  to={REGISTRATION_ROUTE}
+                >
+                  Don't have an account? Register here!
+                </NavLink>
+              )}
+            </div>
+            <div className="auth-form-button-field">
+              <button className="auth-form-login-button">
+                {isRegistration ? "Register" : "Sign in"}
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
