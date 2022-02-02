7bb257e9a47a5fe731923282c91c5b486525e968
btwidc <krauchankakiryl@tut.by>
2022-02-02 15:31:14 +0300

    fix: try to redirect after auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Navigate, Route } from "react-router-dom";
 import AppRouter from "./routes/AppRouter";
 import { Context } from "./index";
 import { observer } from "mobx-react-lite";
+import { MAIN_ROUTE } from "./utils/routesPath";
 
 const App = () => {
   const { userStore } = useContext(Context);
@@ -14,6 +15,10 @@ const App = () => {
 
   return (
     <BrowserRouter>
+      {userStore.isAuth && console.log(userStore)}
+      {userStore.isAuth && (
+        <Route path="*" element={<Navigate to={MAIN_ROUTE} />} />
+      )}
       <AppRouter />
     </BrowserRouter>
   );
--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,17 +1,13 @@
 import React, { useContext } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { publicRoutes } from "./routes";
 import { Context } from "../index";
-import { MAIN_ROUTE } from "../utils/routesPath";
 
 const AppRouter = () => {
   const { userStore } = useContext(Context);
   console.log(userStore);
   return (
     <Routes>
-      {userStore.isAuth && (
-        <Route path="*" element={<Navigate to={MAIN_ROUTE} />} />
-      )}
       {publicRoutes.map(({ path, Component }) => (
         <Route key={path} path={path} element={<Component />} />
       ))}
