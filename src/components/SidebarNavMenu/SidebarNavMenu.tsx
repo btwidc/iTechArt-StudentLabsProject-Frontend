2d7a04ecf680dc52b4994137e50ecdcfe90cfdf2
btwidc <krauchankakiryl@tut.by>
2022-02-12 00:04:14 +0300

    refactor: little appearence changes

--- src/components/SidebarNavMenu/SidebarNavMenu.tsx
+++ src/components/SidebarNavMenu/SidebarNavMenu.tsx
@@ -3,16 +3,10 @@ import "./SidebarNavMenu.scss";
 import { logoutAuthAction } from "../../store/actions/userActions";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-//import AuthService from "../../services/AuthService";
 
 const SidebarNavMenu = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
-  // const test = async () => {
-  //   const response = await AuthService.test();
-  //   console.log(response.data);
-  // };
 
   const handleLogout = async (e: any) => {
     e.preventDefault();
